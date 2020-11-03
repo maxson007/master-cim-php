0575ab8e6d6fc11d458dd36e91e0516c22e5d700
Aymane <danielayman@gmail.com>
2020-11-03 14:47:54 +0100

    ajout de lien sur le tableau couleur

--- Exo/tableau_couleur.php
+++ Exo/tableau_couleur.php
@@ -5,7 +5,13 @@
     <title>Title</title>
 </head>
 <body>
+<?php
+if(isset($_GET['couleur'])){
+    echo "La couleur saisie est :".$_GET['couleur'];
+}
+?>
 <table border="1">
+
 
     <?php
     $couleur = 000000;
@@ -14,7 +20,7 @@
         echo "<tr>\n";
         for ($j = 0; $j < 16; $j++) {
             $couleur = dechex($j) . dechex($j) . dechex($i) . dechex($i) . dechex($k) . dechex($k);
-            echo "<td BGCOLOR='" . $couleur . "'> &nbsp;&nbsp;&nbsp;&nbsp;</td> \n";
+            echo "<td BGCOLOR='" . $couleur . "'><a href=?couleur=$couleur style='text-decoration: none'> &nbsp;&nbsp;&nbsp;&nbsp; </a> </td> \n";
         }
         echo "</tr>\n";
     }
